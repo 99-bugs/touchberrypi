1ccecc6508e77c4970e1dbfe1918cbca03b59c64
Sille Van Landschoot <info@sillevl.be>
2016-05-21 12:37:00 +0000

    update Keyboard class, now accepts code block for key changes

--- lib/touchberrypi/Keyboard.rb
+++ lib/touchberrypi/Keyboard.rb
@@ -1,29 +1,40 @@
 require 'pi_piper'
 require 'qt1070'
-require 'observer'
 
 
 module Touchberrypi
 	class Keyboard
 
 		include PiPiper
-    include Observable
+		include Qt1070
+
+		KEY_MAP = ["LEFT","RIGHT","UP","DOWN","B","A","X"] 
 
 		def initialize
-			sensor = Qt1070::Qt1070.new "/dev/i2c-1"
-			reset_change_signal
-			watch :pin => 4, :pull => :up, :edge => :falling do
-					 key_status = sensor.key_status
-           changed
-           notify_observers(self, miles)
+			sensor = Qt1070.new "/dev/i2c-1"
+
+			# read sensor status to reset 'change' pin
+			sensor.detection_status
+			sensor.key_status
+
+			this = self
+
+			watch :pin => 4, :pull => :up, :trigger => :falling do
+				detection = sensor.detection_status
+				status = sensor.key_status
+				
+				this.call_block	status
 			end
 		end
 
-    private
-    def reset_change_signal
-      sensor.detection_status
-      sensor.key_status
-    end
+		def on_change &block
+			@block = block	
+		end
+
+		def call_block status
+			puts @block.inspect
+			@block.call status unless @block.nil?
+		end
 
 	end
 end
--- test/test.rb
+++ test/test.rb
@@ -2,7 +2,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'touchberrypi'
 
 keyboard = Touchberrypi::Keyboard.new
+keyboard.on_change do |status|
+	puts "key changed #{status}"
+end
 
 loop do
-
+	sleep 0.01
 end
